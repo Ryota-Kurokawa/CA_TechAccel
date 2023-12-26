0cc29c70d6188330ae82d9edadc7c87ff644b2df
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-12-26 15:23:07 +0900

    textfieldから追加　但し、DB接続等は行われない

---
+++ Tech_Accel_P/makeTask.swift
@@ -0,0 +1,18 @@
+//
+//  makeTask.swift
+//  Tech_Accel_P
+//
+//  Created by 黒川良太 on 2023/12/26.
+//
+
+import SwiftUI
+
+struct makeTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    makeTask()
+}
---
+++ Tech_Accel_P/model/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  Tech_Accel_P
+//
+//  Created by 黒川良太 on 2023/12/26.
+//
+
+import Foundation
