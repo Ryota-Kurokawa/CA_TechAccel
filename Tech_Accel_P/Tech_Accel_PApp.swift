8edc22ffc2b50a694a5cebe073fe5cd1d6caf740
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-12-26 14:33:24 +0900

    Initial Commit

---
+++ Tech_Accel_P/Tech_Accel_PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_Accel_PApp.swift
+//  Tech_Accel_P
+//
+//  Created by 黒川良太 on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Tech_Accel_PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
