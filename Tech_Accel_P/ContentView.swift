d8a5dd53fddd36b078b7010146d71bbb91a5e98a
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-12-29 20:54:24 +0900

    localDBに保存

--- Tech_Accel_P/ContentView.swift
+++ Tech_Accel_P/ContentView.swift
@@ -8,39 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State public var tasklist = [Task]()
-    @State var TaskTitle: String = ""
+    @State var newitem = ""
+    @State var TaskList = UserDefaults.standard.array(forKey: "TaskList")
+    
     
     var body: some View {
         VStack {
-            List{
-                ForEach(tasklist) { task in
-                    Text(task.title)
-                }
-                .onDelete(perform: listRemove)
+            HStack{
+                Text("Tech Accel")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+                Spacer()
             }
             
-            TextField("make your task", text: $TaskTitle)
             HStack{
-                Spacer()
+                TextField("new your task", text: $newitem)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300)
                 Button(action: {
-                    let task = Task(title: TaskTitle)
-                    tasklist.append(task)
-                    TaskTitle = ""
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .frame(width: 60, height: 60)
+                    self.TaskList.append(self.newitem)
+                    UserDefaults.standard.set(self.TaskList, forKey: "TaskList")
+                    self.newitem = ""
+                }){
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 5)
+                            .frame(width: 50, height: 30)
+                            .foregroundColor(.green)
+                        Text("add")
+                            .foregroundColor(.white)
+                    }
                 }
             }
+            List {
+                ForEach(TaskList, id: \.self) { task in
+                    Text("\(task)")
+                }
+                .onDelete { indices in
+                    UserDefaults.removeObject(forKey: "TaskList")
+                    TaskList.remove(atOffsets: indices)
+                    UserDefaults.standard.set(TaskList, forKey: "TaskList")
+                }
+            }
+            
+            
+            Spacer()
+        }.onAppear(){
+            guard let defaultItem = TaskList
+            else{return}
+            self.TaskList = defaultItem
+            if self.TaskList != UserDefaults.standard.array(forKey: "TaskList") as? [String] {
+                UserDefaults.standard.removeObject(forKey: "TaskList")
+                UserDefaults.standard.set(self.TaskList, forKey: "TaskList")
+            }
         }
-        .padding()
     }
-    func listRemove(offsets: IndexSet){
-        tasklist.remove(atOffsets: offsets)
+    //    func RemoveTask(offsets: IndexSet){
+    //        TaskList.remove(atOffsets: offsets)
+    //    }
+    
+    func RemoveTask(offsets: IndexSet){
+        TaskList.remove(atOffsets: offsets)
+        UserDefaults.standard.removeObject(forKey: "TaskList")
+        UserDefaults.standard.set(self.TaskList, forKey: "TaskList")
     }
 }
-
 
 #Preview {
     ContentView()
--- Tech_Accel_P/model/Task.swift
+++ Tech_Accel_P/model/Task.swift
@@ -4,15 +4,20 @@
 //
 //  Created by 黒川良太 on 2023/12/26.
 //
-
+//
 import Foundation
 
-struct Task: Identifiable {
-    let id = UUID()
+struct Task: Codable, Identifiable, Hashable {
+
+    static let storeKey = "Task"
+
+    let id: String
     let title: String
-//    let description: String
-//    let date: Date
-//    let isCompleted: Bool
+    let isDone: Bool
+
+    init(id: String = UUID().uuidString, title: String, isDone: Bool) {
+        self.id = UUID().uuidString
+        self.title = title
+        self.isDone = isDone
+    }
 }
-
-
